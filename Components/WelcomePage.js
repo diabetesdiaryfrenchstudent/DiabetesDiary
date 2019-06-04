e3199cd52cef87921470c333394cc3b4555fc125
JD <julie-aurelia1@hotmail.fr>
2019-06-04 10:58:42 +0200

    Primary layout

--- Components/WelcomePage.js
+++ Components/WelcomePage.js
@@ -1,16 +1,41 @@
 // Components/WelcomePage.js
 
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, StyleSheet, Text} from 'react-native'
 
 class WelcomePage extends React.Component {
   render() {
       return (
-        <View>
-          <Text>Hello!</Text>
+        <View style={styles.main_container}>
+          <View style={styles.box}>
+            <Text>Display goals</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Display last data</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Display blood glucose levels</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Display blood glucose graph</Text>
+          </View>
         </View>
       )
   }
 }
+
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: '#a0caec'
+  },
+  box: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    marginTop: 10
+  }
+})
 
 export default WelcomePage
