6ded63cecdee8622ecd3be688702e206a14e6ac4
JD <julie-aurelia1@hotmail.fr>
2019-06-04 09:51:16 +0200

    Files Creation

---
+++ Components/WelcomePage.js
@@ -0,0 +1,16 @@
+// Components/WelcomePage.js
+
+import React from 'react'
+import {View, Text} from 'react-native'
+
+class WelcomePage extends React.Component {
+  render() {
+      return (
+        <View>
+          <Text>Hello!</Text>
+        </View>
+      )
+  }
+}
+
+export default WelcomePage
