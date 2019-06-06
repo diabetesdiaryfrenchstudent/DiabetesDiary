67051bd27742d60fd23503feae30266d6828dd55
JD <julie-aurelia1@hotmail.fr>
2019-06-06 13:09:25 +0200

    About page OK

--- Components/About.js
+++ Components/About.js
@@ -1,16 +1,55 @@
 // Components/About.js
 
 import React from 'react'
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native'
+import {View, ScrollView, StyleSheet, Text, Image, TouchableOpacity, Linking} from 'react-native'
+
+import NSEImage from '../Images/NSEImage.jpg'
 
 class About extends React.Component {
+  static navigationOptions = {
+    title: 'About Diabetes Diary',
+  }
+
   render() {
+
       return (
-        <TouchableOpacity style={styles.main_container}>
-          <View style={styles.box}>
-            <Text>We'll see later</Text>
+        <View style={styles.main_container}>
+        <View style={{flex:2, margin: 5}}>
+
+            <Text style={styles.copyright}>Version:</Text>
+
+            <Text style={styles.copyright}>{'\n'}Diabetes Diary is a result of several research projects at the Noregian Center for E-health Research. Read more on the webpage :</Text>
+            <TouchableOpacity onPress={() => Linking.openURL('http://diabetesdagboka.no')}>
+              <Text style={styles.link}> http://diabetesdagboka.no </Text>
+            </TouchableOpacity>
+            <Text style={styles.copyright}> {'\n'}Keep in mind that Diabetes Diary is storing your health dat on your mobile device. Read more </Text>
+              <TouchableOpacity onPress={() => Linking.openURL('http://www.google.com')}>
+                <Text style={styles.copyright, {textDecorationLine: 'underline', color: '#0837ff'}}>here.</Text>
+              </TouchableOpacity>
+            <Text style={styles.copyright}>Copyright of this software belongs to NST(2013)/NSE(2017).</Text>
+
+            <Text style={styles.copyright}>{'\n'}{'\n'}Norwegian Food Composition Database 2017. Norwegian Food Safety Authority, The Norwegian Directorate of Health and University of Oslo.</Text>
+            <TouchableOpacity onPress={() => Linking.openURL('http://www.matvaretabellen.no')}>
+              <Text style={styles.link}> http://www.matvaretabellen.no. </Text>
+            </TouchableOpacity>
+
+
+            <Text style={styles.copyright}>{'\n'}{'\n'}The following third party components, licensed under Apache License 2.0, are used: android-xmlrpc, AChartEngine, exchangeit, Apache Oltu, jcsv.</Text>
+            <TouchableOpacity onPress={() => Linking.openURL('http://www.apache.org/licenses/LICENSE-2.0.html')}>
+              <Text style={styles.link}> http://www.apache.org/licenses/LICENSE-2.0.html </Text>
+            </TouchableOpacity>
+
+
+            <Text style={styles.copyright}>{'\n'}{'\n'}The following third party components, licensed under MIT License, are used: SeekArc. {'\n'}MIT License:</Text>
+            <Text style={styles.copyright, {fontSize: 8}}>Permission to use, copy, modify, ditribute, and sell this software and its documentation for any purpose is hereby granted without fee,
+            provided that the above copyright notice appear in all copies and that both that copyright notice and this permmision notice appear in supporting documentation. No representations are made about the suitability of this software for any purpose. It is provided as is without express or implied warranty.</Text>
+
           </View>
-        </TouchableOpacity>
+
+          <View style={{flex:1}}>
+            <Image source={NSEImage}  style={{flex:1, height: undefined, width: undefined}}  resizeMode='contain' />
+          </View>
+        </View>
       )
   }
 }
@@ -19,12 +58,15 @@ class About extends React.Component {
 const styles = StyleSheet.create({
   main_container: {
     flex: 1,
-    backgroundColor: '#a0caec'
+    backgroundColor: '#a0caec',
   },
-  box: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    marginTop: 10
+  copyright: {
+    textAlign:'justify',
+  },
+  link: {
+    textDecorationLine: 'underline',
+    color: '#0837ff',
+    textAlign:'center'
   }
 })
 
