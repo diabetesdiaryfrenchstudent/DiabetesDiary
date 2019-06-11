f8cda5621bb4971d277249f1e70fd4f4630e0e2f
neilerua72 <neilerua72@gmail.com>
2019-06-11 16:46:10 +0200

    Check if the name is not empty
    USE A NEW PACKAGE : react-native-material-textfield

--- Components/Welcome/Name.js
+++ Components/Welcome/Name.js
@@ -1,33 +1,97 @@
 import React from 'react'
 import {StyleSheet, View,Text,Button,TouchableOpacity,TextInput} from 'react-native'
 import text from './text'
+import { TextField } from 'react-native-material-textfield';
 import { connect } from 'react-redux'
 
 class Name extends React.Component{
   constructor(props){
     super(props)
-    nameText="";
-  }
+      this.onFocus = this.onFocus.bind(this);
+      console.log("ICI")
+      this.onSubmit = this.onSubmit.bind(this);
+      this.onChangeText = this.onChangeText.bind(this);
+      this.firstnameRef = this.updateRef.bind(this, 'firstname')
+      this.state = {
+      };
+    }
 
+    onFocus() {
+      let { errors = {} } = this.state;
 
-  //Method which update at each new input the name.
-  _nameTextInputChanged(text){
-    this.nameText=text
-  }
+      for (let name in errors) {
+        let ref = this[name];
+
+        if (ref && ref.isFocused()) {
+          delete errors[name];
+        }
+      }
+
+      this.setState({ errors });
+    }
+
+    onChangeText(text) {
+      ['firstname']
+        .map((name) => ({ name, ref: this[name] }))
+        .forEach(({ name, ref }) => {
+          if (ref.isFocused()) {
+            this.setState({ [name]: text });
+          }
+        });
+    }
+
+    onSubmit() {
+      let errors = {};
+
+      ['firstname']
+        .forEach((name) => {
+          let value = this[name].value();
+
+          if (!value) {
+            errors[name] = 'Should not be empty';
+          }else{
+          const action = {type: "TOGGLE_NAME", value: value}
+          //Send to the store the value and the type of action
+          this.props.dispatch(action)
+
+          //Check if the initialization is over
+          if(this.props.param.endInit){
+            this.props.navigation.navigate("Recap")
+          }else{
+            this.props.navigation.navigate("Type")
+          }
+        }
+        });
+
+      this.setState({ errors });
+    }
+
+    updateRef(name, ref) {
+      this[name] = ref;
+    }
+
 
 
   render(){
+    let { errors = {}, ...data } = this.state;
+    let { firstname = 'name'} = data;
     return(
       <View style={styles.main_container}>
       <View style={styles.bulle}>
         <Text style={styles.content}>{text.name}</Text>
-        <TextInput
-          style={styles.textinput}
-          placeholder='Name'
-          onChangeText={(text)=>this._nameTextInputChanged(text)}
-          onSubmitEditing={() => this._buttonClick()}//Possibility to click on OK on the keyboard
-        />
-        <Button title='Ok' onPress={() => this._buttonClick()}/>
+        <TextField
+              ref={this.firstnameRef}
+              value={data.firstname}
+              autoCorrect={false}
+              enablesReturnKeyAutomatically={true}
+              onFocus={this.onFocus}
+              onChangeText={this.onChangeText}
+              onSubmitEditing={this.onSubmit}
+              returnKeyType='next'
+              label='First Name'
+              error={errors.firstname}
+            />
+        <Button title='Ok' onPress={() => this.onSubmit()}/>
       </View>
       </View>
     )
@@ -35,17 +99,12 @@ class Name extends React.Component{
 
   //Action for the button OK
   _buttonClick(){
-    const action = {type: "TOGGLE_NAME", value: this.nameText}
-    //Send to the store the value and the type of action
-    this.props.dispatch(action)
+    let errors={};
 
-    //Check if the initialization is over
-    if(this.props.param.endInit){
-      this.props.navigation.navigate("Recap")
-    }else{
-      this.props.navigation.navigate("Type")
+    if(nameText=""){
+      errors.name='Should not be empty'
     }
-  }
+}
 
 }
 const styles = StyleSheet.create({
