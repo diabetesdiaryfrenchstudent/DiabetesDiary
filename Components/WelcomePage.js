9003a7f3c26cfd2c128dbd46c0f131a31cc12bd4
JD <julie-aurelia1@hotmail.fr>
2019-06-13 17:36:43 +0200

    Help update + icon buttons

--- Components/WelcomePage.js
+++ Components/WelcomePage.js
@@ -2,21 +2,19 @@
 
 import React from 'react'
 import {View, StyleSheet, Text, TouchableOpacity, Button, Image} from 'react-native'
-import Icon from "react-native-vector-icons/AntDesign"
 import { DrawerActions } from 'react-navigation'
 import Modal from 'react-native-modal';
-import ImageResizeMode from 'react-native/Libraries/Image/ImageResizeMode'
+
+import AntDesignIcon from 'react-native-vector-icons/AntDesign'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 import styles from '../Tools/Styles'
-import addBloodDataIcon from '../Images/created/add_blood.png'
-import addActivityDataIcon from '../Images/created/add_activity.png'
-import addCaloriesDataIcon from '../Images/created/add_calories.png'
-import addCarbohydratesDataIcon from '../Images/created/add_carbohydrates.png'
-import addInsulinDataIcon from '../Images/created/add_insulin.png'
-import addMedicationDataIcon from '../Images/created/add_medication.png'
-import addWeightDataIcon from '../Images/created/add_weight.png'
 
+import insulinIcon from '../Images/insulin_syringe.png'
 import h1 from '../Images/Help/tutorial_homescreen.png'
+
+
 
 class WelcomePage extends React.Component {
   static navigationOptions = {
@@ -32,25 +30,8 @@ class WelcomePage extends React.Component {
   }
 
 
-  _renderTabButton = (text, onPress) => (
-    <TouchableOpacity onPress={onPress}>
-      <View style={styles.button}>
-        <Text style={styles.textButton}>{text}</Text>
-      </View>
-    </TouchableOpacity>
-  );
 
-  _renderAddButton = (onPress) => (
-    <TouchableOpacity onPress={onPress}>
-      <View style={styles.CircleShapeView}>
-        <Icon name='pluscircleo'
-        type='AntDesign'
-        style={{fontSize:50}}
-        />
-      </View>
-    </TouchableOpacity>
-  );
-  _renderAddDataButton = (logosrc, onPress ) => (
+  _renderAddInsulinDataButton = (logosrc, onPress ) => (
     <TouchableOpacity onPress={onPress}>
       <View style={styles.CircleShapeView}>
         <Image source={logosrc} style={{height: 35, width: 35}} />
@@ -58,20 +39,29 @@ class WelcomePage extends React.Component {
     </TouchableOpacity>
   );
 
+  _renderIconButton = (TypeIcon, name, onPress ) => (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.CircleShapeView}>
+        <TypeIcon name={name} style={{fontSize:40}} />
+      </View>
+    </TouchableOpacity>
+  );
 
 
 
   _renderModalContentAdd = () => (
     <View style={{flexDirection: 'row-reverse', justifyContent: 'center', marginBottom: 40}}>
-      {this._renderAddDataButton(addBloodDataIcon, () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
-      {this._renderAddDataButton(addInsulinDataIcon, () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
-      {this._renderAddDataButton(addCaloriesDataIcon, () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
-      {this._renderAddDataButton(addActivityDataIcon, () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
-      {this._renderAddDataButton(addWeightDataIcon, () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
-      {this._renderAddDataButton(addMedicationDataIcon, () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
+      {this._renderIconButton(FontAwesome, 'tint', () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
+      {this._renderAddInsulinDataButton(insulinIcon, () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
+      {this._renderIconButton(MaterialCommunityIcons, 'food-fork-drink', () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
+      {this._renderIconButton(MaterialCommunityIcons, 'bike', () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
+      {this._renderIconButton(MaterialCommunityIcons, 'scale-bathroom', () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
+      {this._renderIconButton(MaterialCommunityIcons, 'pill', () => {this.props.navigation.navigate('AddData'), this.setState({ visibleModal: null})})}
+
     </View>
   );
 
+//not used yet bc of unworking header button
 _helpContent = () => (
   <View>
     <TouchableOpacity style={{flex:1, justifyContent: 'flex-end', marginRight:10}} onPress={()=>this.setState({visibleModal:null})}>
@@ -95,7 +85,12 @@ _helpContent = () => (
           </TouchableOpacity>
 
           <View style={{flexDirection: 'row', justifyContent: 'flex-end', marginRight: 28}}>
-            {this._renderAddButton(() => this.setState({ visibleModal: 2 }))}
+          <TouchableOpacity onPress={ () => this.setState({ visibleModal: 2 })} >
+            <View style={styles.CircleShapeView}>
+              <AntDesignIcon name='pluscircleo' style={{fontSize:50}} />
+            </View>
+          </TouchableOpacity>
+
             <Modal backdropColor={'rgba(0, 0, 0, 0.50)'} onRequestClose={() => {this.setState({ visibleModal: null})}} isVisible={this.state.visibleModal === 2} style={styles.bottomModal}>
               <View>
                 <TouchableOpacity style={{flex:1, justifyContent: 'flex-end', marginRight:10}} onPress={()=>this.setState({visibleModal:null})}>
